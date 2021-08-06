eb626e9963f0151279523db5077a98e11e078843
jgarza9788 <jgarza9788@gmail.com>
2021-08-06 00:16:22 -0700

    20210806

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# VAERS
- 
+# VAERS  
+
+
+<!--TOC-->
+* [VAERS](#vaers)
+	* [Disclaimer](#disclaimer)
+	* [get started](#get-started)
+		* [1. download the data from VAERS website](#1.-download-the-data-from-vaers-website)
+		* [2. move the AllVAERSDataCSVS.zip to VAERS folder and unzip it](#2.-move-the-allvaersdatacsvs.zip-to-vaers-folder-and-unzip-it)
+		* [3. check the files](#3.-check-the-files)
+		* [4. run main() from VAERS.py](#4.-run-main()-from-vaers.py)
+
+<!--TOC-->
+## Disclaimer
+**THIS IS NOT MEDICAL ADVICE**  
+if you want medical advice please go see your doctor.
+
+please also read the disclaimer on the VAERS data on their website. 
+[VAERS website](https://vaers.hhs.gov/data.html)
+
+## get started
+### 1. download the data from VAERS website
+
+[link to VAERS website](https://vaers.hhs.gov/data/datasets.html?)
+
+this will give you AllVAERSDataCSVS.zip
+
+### 2. move the AllVAERSDataCSVS.zip to VAERS folder and unzip it
+
+### 3. check the files
+
+you should have at least 6 files   
+```
+.../VAERS/AllVAERSDataCSVS/  
+    2021VAERSVAX.csv  
+    2021VAERSSYMPTOMS.csv  
+    2021VAERSDATA.csv  
+    2020VAERSVAX.csv  
+    2020VAERSSYMPTOMS.csv  
+    2020VAERSDATA.csv  
+```
+
+### 4. run main() from VAERS.py
+
+this will merge the files above (if it's the first time running).
+and print out some stats about the data.  
+
--- VAERS.py
+++ VAERS.py
@@ -17,19 +17,28 @@ pd.set_option('display.max_rows', None)
 
 
 def get_file_list(path):
+    """
+    returns a list of all the files in the given path/directory
+    """
     return [ os.path.join(path,f) for f in os.listdir(path)]
 
-def compile_data_all_VAERSDATA():
-    csv_files = get_file_list(datapath)
-    data_files = [f for f in csv_files if re.search(r'\d{4}VAERSDATA',f)]
-    df = pd.DataFrame()
-    for i,f in enumerate(data_files):
-        print(i,f)
-        df0 = pd.read_csv(f,encoding='cp1252')
-        df = pd.concat([df,df0])
-    return df
+# def compile_data_all_VAERSDATA():
+#     """
+#     compiles 
+#     """
+#     csv_files = get_file_list(datapath)
+#     data_files = [f for f in csv_files if re.search(r'\d{4}VAERSDATA',f)]
+#     df = pd.DataFrame()
+#     for i,f in enumerate(data_files):
+#         print(i,f)
+#         df0 = pd.read_csv(f,encoding='cp1252')
+#         df = pd.concat([df,df0])
+#     return df
 
 def compile_files(directory,files):
+    """
+    compiles/adds/unions multiple csv files together and returns a dataframe
+    """
     df = pd.DataFrame()
     for i,f in enumerate(files):
         # print(i,f)
@@ -39,31 +48,46 @@ def compile_files(directory,files):
 
 
 def load_json(path_to_file):
+    """
+    loads json from file
+    """
     with open(path_to_file, 'r') as f:
         return json.load(f)
 
 
 def save_json(path_to_file,data):
+    """
+    writes dict/json to file
+    """
     with open(path_to_file, 'w') as f:
         json.dump(data, f)
 
 def has_covid(text):
+    """
+    returns 1 or 0 if text has \'COVID\' in it
+    """
     if re.search('COVID',text.upper()):
         return 1
     else:
         return 0
 
 def process_to_one_file():
-    print('process to one file\n\n')
+    """
+    processes VAERS data from 2020 and 2021, creates all_data.json, and returns a dataframe
+    """
+    print('process to one file\n\tthis might take a while...go get a drink☕🍷🍸🍹🍶🍺\n')
     df_data = compile_files(datapath,['2020VAERSDATA.csv','2021VAERSDATA.csv'])
     df_vax = compile_files(datapath,['2020VAERSVAX.csv','2021VAERSVAX.csv'])
     df_sym = compile_files(datapath,['2020VAERSSYMPTOMS.csv','2021VAERSSYMPTOMS.csv'])
 
+    print("""
+    symptoms are contained in columns (up to 5 symptoms per event)
+    we must transform these symptoms into a single list for each event
+    """)
     print('dedup-ing Symptoms')
     vid = list(df_sym['VAERS_ID'].unique())
 
     idf_sym = []
-    # for index,v in enumerate(vid[0:100]):
     for index,v in enumerate(vid):
         if index%100 == 0:
             print('{:.2f}'.format(index/len(vid)))
@@ -84,11 +108,9 @@ def process_to_one_file():
     df = df.drop_duplicates(ignore_index = True)
     df = pd.merge(df,df_sym,how='outer',on='VAERS_ID')
 
-
     df.reset_index()
-    # df = df.head(25)
 
-    # print(len(df))
+    # creating a new column depending if this is the covid vaccine or not
     df['COVID_VAX'] = df['VAX_TYPE'].apply(has_covid)
     df = df[df['COVID_VAX'] == 1]
     # print(len(df))
@@ -96,7 +118,7 @@ def process_to_one_file():
     print('all columns\n',df.columns)
     print('top 50\n',df.head(50))
 
-    f0 = os.path.join(datapath,'all_data.csv')
+    f0 = os.path.join(datapath,'all_data.csv') #not really needed...but some people might like a csv
     f1 = os.path.join(datapath,'all_data.json')
     df.to_csv(f0)
     save_json(f1,df.to_dict(orient='records'))
@@ -106,35 +128,10 @@ def process_to_one_file():
     return df
 
 
-def break_down(_df,column,q=[0,0.125,0.25,0.375,0.5,0.625,0.75,0.875,1]):
-    print('\nbreak down of {0}'.format(column))
-    df = pd.DataFrame(_df[column])
-    bucket_column = column + '_bucket'
-    df[bucket_column] = pd.qcut(
-            df[column],
-            q=q,
-            labels=False,
-            precision=0,
-            duplicates='drop',
-        )
-
-    # print(df.head())
-
-    dmin = df.groupby(by=bucket_column).min()
-    dmin = dmin.rename(columns={column: column + '_min'})
-    dmax = df.groupby(by=bucket_column).max()
-    dmax = dmax.rename(columns={column: column + '_max'})
-    dcount = df.groupby(by=bucket_column).count()
-    dcount = dcount.rename(columns={column: column + '_count'})
-
-    df0 = pd.merge(dmin,dmax,how='outer',on=bucket_column)
-    df0 = pd.merge(df0,dcount,how='outer',on=bucket_column)
-
-    df0['percent'] = (df0[column + '_count']/df0[column + '_count'].sum()) * 100
-
-    print(df0)
-
 def break_down_2(_df,column):
+    """
+    shows what values there are for a given column (with counts and percent)
+    """
     print('\nbreak down of {0}'.format(column))
     df = pd.DataFrame(_df[column])
     df = df.fillna('nan')
@@ -146,8 +143,10 @@ def break_down_2(_df,column):
         df0 = df[df[column]==i]
         print(column.ljust(10),'\t',str(i).ljust(10),'\t',str(len(df0)).ljust(10),'\t','{:.2f}'.format((len(df0)/len(df))*100).ljust(10))
 
-
 def process_symptoms_to_list(df):
+    """
+    returns a list of symptoms for the dataframe
+    """
     s = df['SYMPTOMS'].to_list()
     l = []
     for i in s:
@@ -162,6 +161,9 @@ def process_symptoms_to_list(df):
     return l
 
 def break_down_3(_df,column,buckets,message=''):
+    """
+    breaks a column down into buckets/bins
+    """
     print('\n\n',message,'\ncolumn: ',column, '\n','buckets: ', buckets)
     
     df = pd.DataFrame(_df[column])
@@ -171,20 +173,19 @@ def break_down_3(_df,column,buckets,message=''):
     df['percent'] = df['percent'].round(2)
     print(df)
 
+def symptom_list(df,top=100):
+    """
+    displays a list of the most popular symptoms
+    note: symptoms might be medical jargon or plain english
+    i.e. \"RASH\",\"ERYTHEMA\", and \"ITCHY RED SKIN\" would be reported as different items (for now)
+    """
 
-
-if __name__ == '__main__':
-    #settings
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
-    # pd.set_option('display.max_colwidth', 0)
-
-    # df = process_to_one_file()
-
-    df = load_json(os.path.join(datapath,'all_data.json'))
-    df = pd.DataFrame(df)
-    print(df.columns)
-
+    print(
+"""
+note: symptoms might be medical jargon or plain english
+i.e. \"RASH\",\"ERYTHEMA\", and \"ITCHY RED SKIN\" would be reported as different items
+"""
+    )
 
     symptoms =  process_symptoms_to_list(df)
     symp_count = len(symptoms)
@@ -192,75 +193,172 @@ if __name__ == '__main__':
     symptoms = symptoms.most_common()
     # print(symptoms)
 
-    print('\ntop 100 symptoms')
+    print('\ntop {top} symptoms'.format(top=top))
     cs = [4,24,12,12]
     print('#'.ljust(cs[0]),'symptom'.ljust(cs[1]),'value'.ljust(cs[2]),'percent'.ljust(cs[3]))
-    for index,i in enumerate(symptoms[0:100]):
+    for index,i in enumerate(symptoms[0:top]):
         print(
             str(index).ljust(cs[0]),
             str(i[0][0:cs[1]]).ljust(cs[1]),
             str(i[1]).ljust(cs[2]),
             '{:.2f}'.format((i[1]/symp_count)*100).ljust(cs[3])
         )
+    
+    return symptoms
 
+def get_data():
+    """
+    gets the data and returns a dataframe
+    """
+
+    all_data = os.path.join(datapath,'all_data.json')
+
+    if os.path.isfile(all_data):
+        print('loading all_data.json')
+        df = load_json(all_data)
+        df = pd.DataFrame(df)
+    else:
+        print('processing the 2020-2021 files')
+        print("""
+        .../VAERS/AllVAERSDataCSVS/  
+            2021VAERSVAX.csv  
+            2021VAERSSYMPTOMS.csv  
+            2021VAERSDATA.csv  
+            2020VAERSVAX.csv  
+            2020VAERSSYMPTOMS.csv  
+            2020VAERSDATA.csv 
+        """)
+        df = process_to_one_file()
+
+    print(df.columns)
+
+    return df
+
+
+def breakdowns(df):
+    """
+    breaks down the data
+    """
     ddf = df[df['DIED']=='Y']
 
-    print('\n\n')
     break_down_3(df,'AGE_YRS',[0,15,25,35,45,55,65,75,85,500])
-    break_down_3(ddf,'AGE_YRS',[0,15,25,35,45,55,65,75,85,500],message='deaths only')
+    break_down_3(ddf,'AGE_YRS',[0,15,25,35,45,55,65,75,85,500],message='***deaths only***')
 
     break_down_3(df,'NUMDAYS',[0,10,20,30,40,50,60])
-    break_down_3(ddf,'NUMDAYS',[0,10,20,30,40,50,60],message='deaths only')
+    break_down_3(ddf,'NUMDAYS',[0,10,20,30,40,50,60],message='***deaths only***')
 
     break_down_2(df,'DIED')
     break_down_2(df,'ER_VISIT')
     break_down_2(df,'L_THREAT')
     break_down_2(df,'RECOVD')
 
-    full_VAX = 165*10**6 #from google 8/3/2021
-    half_VAX = 191*10**6 #from google 8/3/2021
+
+
+def percentages_2(vaers_min,vaers_max,vaers_label,vax_count,vax_label):
+    """
+    prints the percents
+    """
+    print(
+        '\n( {vaers_label} / {vax_label} ) * 100\n'.format(
+            vaers_label=vaers_label,
+            vax_label=vax_label
+            ),
+        '{:.2f}'.format((vaers_min/vax_count)*100),
+        ' - ',
+        '{:.2f}'.format((vaers_max/vax_count)*100),
+        '\n'
+        )        
+
+def percentages(df,vax_count,vax_label,vaers_min_adj=80,vaers_max_adj=120):
+    """
+    calculates and prints the percentages
+    """
+
+    print("""
+VAERS only contains reported data and     
+\'...fewer than 1% of vaccine adverse events are reported.\'
+resources:
+https://digital.ahrq.gov/sites/default/files/docs/publication/r18hs017045-lazarus-final-report-2011.pdf (page 6)
+    """)
+
+    print('therfore, will be multiplying the VAERS counts by {0} and {1}'.format(vaers_min_adj,vaers_max_adj))
+    print('thus providing min and max percentages.')
+
     VAERS_count = len(df)
     VAERS_death_count = len(df[df['DIED']=='Y'])
     VAERS_nrecovd_count = len(df[df['RECOVD']=='N'])
 
-    vc_min = VAERS_count * 80
-    vc_max = VAERS_count * 120
+    vc_min = VAERS_count * vaers_min_adj
+    vc_max = VAERS_count * vaers_max_adj
 
-    vdc_min = VAERS_death_count * 80
-    vdc_max = VAERS_death_count * 120
+    vdc_min = VAERS_death_count * vaers_min_adj
+    vdc_max = VAERS_death_count * vaers_max_adj
 
-    vnr_min = VAERS_nrecovd_count * 80
-    vnr_max = VAERS_nrecovd_count * 120
+    vnr_min = VAERS_nrecovd_count * vaers_min_adj
+    vnr_max = VAERS_nrecovd_count * vaers_max_adj
+
+    percentages_2(
+        VAERS_count * vaers_min_adj,
+        VAERS_count * vaers_max_adj,
+        'adverse reaction',
+        vax_count,
+        vax_label
+        )
+    percentages_2(
+        VAERS_death_count * vaers_min_adj,
+        VAERS_death_count * vaers_max_adj,
+        'adverse death',
+        vax_count,
+        vax_label
+        )
+    percentages_2(
+        VAERS_nrecovd_count * vaers_min_adj,
+        VAERS_nrecovd_count * vaers_max_adj,
+        'no recovery',
+        vax_count,
+        vax_label)
+
+
+    
+
+def main():
+    """
+    this will do all the things
+    """
+
+    #this might not work on linux or macOS
+    try:
+        os.system('cls')
+    except:
+        pass
+
+    #settings
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+
+    df = get_data()
 
     print('\n\n--------------------------------\n\n')
 
-    print('\nadverse rections / full vax\n',
-        '{:.2f}'.format((vc_min/full_VAX)*100),
-        ' - ',
-        '{:.2f}'.format((vc_max/full_VAX)*100))
-    print('\nadverse death / full vax\n',
-        '{:.2f}'.format((vdc_min/full_VAX)*100),
-        ' - ',
-        '{:.2f}'.format((vdc_max/full_VAX)*100))
-    print('\nno recovery / full vax\n',
-        '{:.2f}'.format((vnr_min/full_VAX)*100),
-        ' - ',
-        '{:.2f}'.format((vnr_max/full_VAX)*100))
-
+    symptoms = symptom_list(df,top=100)
 
     print('\n\n--------------------------------\n\n')
 
-    print('\nadverse rections / half vax\n',
-        '{:.2f}'.format((vc_min/half_VAX)*100),
-        ' - ',
-        '{:.2f}'.format((vc_max/half_VAX)*100))
-    print('\nadverse death / half vax\n',
-        '{:.2f}'.format((vdc_min/half_VAX)*100),
-        ' - ',
-        '{:.2f}'.format((vdc_max/half_VAX)*100))
-    print('\nno recovery / half vax\n',
-        '{:.2f}'.format((vnr_min/half_VAX)*100),
-        ' - ',
-        '{:.2f}'.format((vnr_max/half_VAX)*100))
+    breakdowns(df)
+
+    print('\n\n--------------------------------\n\n')
+
+    full_VAX = 165*10**6 #from google 8/3/2021
+    print('according to a quick google search (on 8/3/2021) {0:,} have had two doses of the vaccine (full_vax)'.format(full_VAX))
+    percentages(df,full_VAX,'full_vax',80,120)
+
+    print('\n\n--------------------------------\n\n')
+
+    half_VAX = 191*10**6 #from google 8/3/2021
+    print('according to a quick google search (on 8/3/2021) {0:,} have had (at least) one dose of the vaccine (half_vax)'.format(half_VAX))
+    percentages(df,half_VAX,'half_vax',80,120)
 
 
+if __name__ == '__main__':
+
+    main()
